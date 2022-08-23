6035052cf9a6daba1408b3d551d6f01e26cddc25
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2022-08-23 05:04:41 -0700

    updated index.js, declared prompt functions

--- index.js
+++ index.js
@@ -7,53 +7,101 @@
 // }
 
 // [End Here: Use window.alert instead of window.prompt]
-// Prompt: Do you head left or right?
+// Prompt: Do you head left or right? firstAnswer
 // Left
-    // Prompt: Follow or Continue
+    // Prompt: Follow or Continue secondAnswer
     // Follow
-        // Prompt: Stay or Spread the Word
+        // Prompt: Stay or Spread the Word thirdAnswer
         // Stay [End Here]
         // Spread the Word [End Here]
     // Continue
-        // Prompt: Ladder or Staircase
+        // Prompt: Ladder or Staircase thirdAnswer
         // Ladder [End Here]
         // Staircase [End Here]
 // Right
-    // Prompt: Past the Dragon or Away from the Dragon
+    // Prompt: Past the Dragon or Away from the Dragon secondAnswer
     // Past the Dragon
-        // Prompt: Stay or Run
-            // Stay [End Here]
-            // Run [End Here]
+        // Prompt: Stay or Run thirdAnswer
+        // Stay [End Here]
+        // Run [End Here]
     // Away from the Dragon
-        // Prompt: Draw It or Pick It
-            // Draw It [End Here]
-            // Pick It [End Here]
+        // Prompt: Draw It or Pick It thirdAnswer
+        // Draw It [End Here]
+        // Pick It [End Here]
 
-function start(){
-    let firstAnswer  = window.prompt('Do you head left, or right?')
-    if(firstAnswer === 'left'){
-       goLeft()
-    } else if(firstAnswer === 'right') {
+function start() {
+    let firstAnswer = window.prompt('Do you head left, or right?')
+    if (firstAnswer === 'left') {
+        goLeft()
+    } else if (firstAnswer === 'right') {
         goRight()
     }
 }
 
-function goLeft(){
+function goLeft() {
     let secondAnswer = window.prompt(`You come across a stray cat. 
     It scampers off down a small hole, just large enough for you to crawl through. 
     Do you follow it, or continue on your path?`)
-    if(secondAnswer === 'back'){
+    if (secondAnswer === 'back') {
         start()
     }
 }
 
-function goRight(){
+function followCat() {
+
+}
+
+function stayCat() {
+
+}
+
+function spreadWord() {
+
+}
+
+function continuePath() {
+
+}
+
+function ladder() {
+
+}
+
+function staircase() {
+
+}
+
+function goRight() {
     let secondAnswer = window.prompt(`You come across a snoring dragon. 
     On the other side of him, you see a shiny chest of treasure. Another path would 
     lead you away from the dragon all together. Which do you take?`)
-    if(secondAnswer === 'back'){
+    if (secondAnswer === 'back') {
         start()
     }
+}
+
+function pastDragon() {
+
+}
+
+function stayDragon() {
+
+}
+
+function run(){
+
+}
+
+function awayDragon() {
+
+}
+
+function drawIt() {
+
+}
+
+function pickIt() {
+    
 }
 
 start()
